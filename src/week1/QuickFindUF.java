0afd0270360933e16856f8b86936ab6873f8f489
xsenechal <xsenechal@gmail.com>
2013-09-04 14:42:02 +0200

    Update QuickFind

--- src/week1/QuickFindUF.java
+++ src/week1/QuickFindUF.java
@@ -7,8 +7,19 @@ import stdlib.StdIn;
  * Date: 04/09/13 - 13:21
  */
 public class QuickFindUF {
+    private int[] id;
+
+    public QuickFindUF(int N) {
+        id = new int[N];
+        for (int i = 0; i < N; i++){
+            id[i] = i;
+        }
+    }
+
     public static void main(String[] args) {
         int N = StdIn.readInt();
+        QuickFindUF uf = new QuickFindUF(N);
         System.out.println(N);
+        System.out.println(uf);
     }
 }
