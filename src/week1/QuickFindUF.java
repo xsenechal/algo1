7234d7bb85d3db32d29735305240514b79ac72ac
xsenechal <xsenechal@gmail.com>
2013-09-04 13:45:27 +0200

    Add stdlib

---
+++ src/week1/QuickFindUF.java
@@ -0,0 +1,14 @@
+package week1;
+
+import stdlib.StdIn;
+
+/**
+ * User: xs86768
+ * Date: 04/09/13 - 13:21
+ */
+public class QuickFindUF {
+    public static void main(String[] args) {
+        int N = StdIn.readInt();
+        System.out.println(N);
+    }
+}
